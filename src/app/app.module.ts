d4e2347c91f36679dedd0610c45c2412dc5478a6
blhsrwznrghfzpr <blhsrwznrghfzpr.hompyhrln@gmail.com>
2019-02-05 21:50:49 +0900

    feat: plugin-charazip作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -57,6 +57,8 @@ import { TabletopService } from 'service/tabletop.service';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { CharazipComponent } from './plugins/charazip/charazip.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -64,6 +66,7 @@ import { AppRoutingModule } from './app-routing.module';
     CardComponent,
     CardStackComponent,
     CardStackListComponent,
+    CharazipComponent,
     ChatMessageComponent,
     ChatPaletteComponent,
     ChatTabComponent,
